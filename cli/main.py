5f1f8ed24977a59bfaa97f0cbd38bd046c252348
Ashot Tadevosyan <ashottadevosyan27@gmail.com>
2025-03-25 19:18:48 +0400

    add error-handling to the main

--- cli/main.py
+++ cli/main.py
@@ -1,13 +1,33 @@
 
-from ..db_loader import load_names_from_db
-from ..search_engine import find_best_matches
+from search.db_loader import load_names_from_db
+from search.search_engine import find_best_matches
 
 
 def main():
     input_name = input("Enter a name to search: ").strip()
-    db_names = load_names_from_db()
 
-    matches = find_best_matches(input_name, db_names)
+    if not input_name:
+        print("Error: Name cannot be empty.")
+        return
+    
+    if len(input_name) < 3:
+        print("Error: Name must be at least 3 characters long.")
+        return
+    try:
+        db_names = load_names_from_db()
+    except Exception as e:
+        print(f"Error loading database: {e}")
+        return
+
+    try:
+        matches = find_best_matches(input_name, db_names)
+    except Exception as e:
+        print(f"Error during matching: {e}")
+        return
+
+    if not matches:
+        print("No matches found.")
+        return
 
     print("\nTop Matches:")
     for name, score in matches:
