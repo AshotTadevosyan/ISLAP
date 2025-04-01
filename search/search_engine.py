4aeb22eaaa178aaf353d1f3c03056ab1c1189b5f
Ashot Tadevosyan <ashottadevosyan27@gmail.com>
2025-04-01 17:22:16 +0400

    Applied normalization in search_engine to improve accuracy

--- search/search_engine.py
+++ search/search_engine.py
@@ -1,10 +1,16 @@
 from algorithms.scorer import combined_score
+from algorithms.utility import normalize  
 
 def find_best_matches(input_name, db_names, threshold=0.6):
     matches = []
+    input_name = normalize(input_name) 
+
     for name in db_names:
-        score = combined_score(input_name, name)
+        normalized_name = normalize(name)  
+        score = combined_score(input_name, normalized_name)
+
         if score >= threshold:
-            matches.append((name, score))
+            matches.append((name, score))  
+
     matches.sort(key=lambda x: x[1], reverse=True)
     return matches[:5]
