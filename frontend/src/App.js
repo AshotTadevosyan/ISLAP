4eb8e7af2394481af59e459c3309c048cd061b4b
Ashot Tadevosyan <ashottadevosyan27@gmail.com>
2025-04-09 19:59:23 +0400

    Update frontend to use full backend URL

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -27,7 +27,7 @@ function App() {
 
     try {
       const response = await Axios.get(
-        `${process.env.REACT_APP_API_URL}/https://api.render.com/deploy/srv-cvkpga24d50c73du95c0?key=CFDjXvp1Uv8`,
+        `${process.env.REACT_APP_API_URL}/search`,
         {
           params: {
             name: fullName,
